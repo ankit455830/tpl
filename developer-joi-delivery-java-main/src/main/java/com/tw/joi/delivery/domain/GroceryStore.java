ee8d288b5b62d345a68bdedbf77b31a46411090e
ankit455830 <ankitmotani35@gmail.com>
2026-06-16 21:25:06 +0530

    seeddata-refactored

--- developer-joi-delivery-java-main/src/main/java/com/tw/joi/delivery/domain/GroceryStore.java
+++ developer-joi-delivery-java-main/src/main/java/com/tw/joi/delivery/domain/GroceryStore.java
@@ -1,17 +1,11 @@
 package com.tw.joi.delivery.domain;
 
-import java.util.HashSet;
-import java.util.Set;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class GroceryStore extends Outlet {
 
     @Builder
